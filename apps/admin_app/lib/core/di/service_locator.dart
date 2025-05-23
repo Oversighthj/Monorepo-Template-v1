91f3e3dba2eda354c355960bb226d86145021bd4
Oversighthj <139948909+Oversighthj@users.noreply.github.com>
2025-05-23 12:43:31 +0800

    Update service_locator.dart

--- apps/admin_app/lib/core/di/service_locator.dart
+++ apps/admin_app/lib/core/di/service_locator.dart
@@ -1,21 +1,18 @@
 import 'package:get_it/get_it.dart';
-import 'package:connectivity_plus/connectivity_plus.dart';
-
-import '../network/api_client.dart';
-import '../network/network_info.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+import 'core/network/api_client.dart';
 
 final sl = GetIt.instance;
 
-/// تهيئة جميع الـ singleton المستخدمة في admin_app
-Future<void> initServiceLocator() async {
-  // اتصال الشبكة (Connectivity)
-  sl.registerLazySingleton(() => Connectivity());
+Future<void> setupLocator() async {
+  final prefs = await SharedPreferences.getInstance();
 
-  // كلاس فحص الشبكة
-  sl.registerLazySingleton<NetworkInfo>(
-    () => NetworkInfoImpl(connectivity: sl()),
-  );
+  // register only ONCE – no duplicates
+  sl.registerLazySingleton<ApiClient>(() {
+    final client = ApiClient();
+    client.setToken(prefs.getString('auth_token'));
+    return client;
+  });
 
   // ApiClient الموحّد مع تحميل التوكن
   final prefs = await SharedPreferences.getInstance();
