40ade71e8552d1460d42493ae1ef3f5392005966
Oversighthj <139948909+Oversighthj@users.noreply.github.com>
2025-05-23 08:41:48 +0800

    Adjust security for tests

--- services/backend/src/test/java/com/example/app/message/MessageServiceTest.java
+++ services/backend/src/test/java/com/example/app/message/MessageServiceTest.java
@@ -16,6 +16,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
 @DataJpaTest
+@org.springframework.test.context.ActiveProfiles("test")
 class MessageServiceTest {
 
   @Autowired MessageRepository messageRepo;
